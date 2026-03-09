d2c77baa55b2b71b39602793d6fca6bdcf40bbd5
GabrielZanini04 <gabrielzanini03@gmail.com>
2026-03-09 15:50:37 -0300

    adiciona funcionalidades de criação e edição de tarefas, incluindo validação de entrada e mensagens de sucesso

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -5,6 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Task
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $description
+ * @property string|null $long_description
+ * @property bool $completed
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ */
 class Task extends Model
 {
     use HasFactory;
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,12 +3,16 @@
 
 <head>
     <title>Laravel 10 task list</title>
+    @yield('styles')
 </head>
 
 <body>
     <h1>@yield('title')</h1>
 
     <div>
+        @if (session()->has('success'))
+        <div>{{ session('success') }}</div>
+        @endif
         @yield('content')
     </div>
 </body>
