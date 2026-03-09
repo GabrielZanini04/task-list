78bdb52748c73c348be00593aa07993f37593a6f
GabrielZanini04 <gabrielzanini03@gmail.com>
2026-03-09 19:08:23 -0300

    adiciona validação de entrada para criação e edição de tarefas, refatorando rotas e métodos para usar o TaskRequest

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -5,18 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-/**
- * App\Models\Task
- *
- * @property int $id
- * @property string $title
- * @property string $description
- * @property string|null $long_description
- * @property bool $completed
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- */
 class Task extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title', 'description', 'long_description'] ;
+    //protected $guarded = ['secret'];
 }
